3778b056e1771fb45d2c2babb2e0768b29b5d9b7
Dawid Alimowski <dawid.alimowski@gmail.com>
2021-12-26 15:39:14 +0100

    init

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MyApp from './App';
+
+test('renders learn react link', () => {
+  render(<MyApp />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
