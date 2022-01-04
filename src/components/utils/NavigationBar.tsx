b726ab28a607c3ba882072a9d780c3bd6c70579d
witek0509x <witek.droch@gmail.com>
2022-01-04 11:20:34 +0100

    basic Navbar Structure

--- src/components/utils/NavigationBar.tsx
+++ src/components/utils/NavigationBar.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { RiHome2Line, RiUserLine } from "react-icons/ri";
+import {useNavigate} from "react-router-dom";
+import {RiHome2Line, RiUserLine} from "react-icons/ri";
 
 const NavigationBar = () => {
 
     const navigate = useNavigate();
 
-  return (
-    <>
-      <nav className="nav">
-        <div className="align-left">
-          <RiHome2Line className="icon" onClick={() => navigate("cars")}/>
-        </div>
-        Navigation
-        <div className="align-right">
-          <RiUserLine className="icon" onClick={() => navigate("login")}/>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav className="nav">
+                <div className="align-left">
+                    <p>
+                        Carly Admin
+                    </p>
+                    <button>
+                        Filter
+                    </button>
+                    <button>
+                        Sort
+                    </button>
+
+
+
+                </div>
+                <input />
+                <button>
+                    New Car
+                </button>
+                <div className="align-right">
+                    <button>Bookings</button>
+                    <RiUserLine className="icon" onClick={() => navigate("login")}/>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default NavigationBar;
