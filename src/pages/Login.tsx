0fb6304551f441d424fa99c05e04c69bfa9af951
witek0509x <witek.droch@gmail.com>
2022-01-04 15:54:00 +0100

    basic login mocked

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { AuthState, login, logout } from "../store/AuthSlice";
+import logo from "../resources/logo_login.bmp"
+import "../styles/login.css"
 
 const Login = () => {
   const isLoggedIn = useSelector((state: {auth: AuthState}) => state.auth.isLoggedIn);
@@ -14,8 +16,19 @@ const Login = () => {
 
   return (
     <>
-      <h1>Login</h1>
-      <button onClick={onClick}>{isLoggedIn ? "Log out" : "Log in"}</button>
+        <form className={"login-container"}>
+            <img src={logo} className={"logo"}/>
+
+            <input placeholder={"username"}/>
+            <input placeholder={"password"} type={"password"}/>
+
+            <button
+                onClick={onClick}
+                className={"login-button"}>
+                Log in
+            </button>
+        </form>
+
     </>
   );
 };
