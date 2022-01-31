ab37e0d8a394d0855face122f45b0498bcd27439
Magda <magdalenafalkowska00@wp.pl>
2022-01-31 12:48:01 +0100

    Booking sort styling

--- src/components/utils/navbar/BookingsSort.tsx
+++ src/components/utils/navbar/BookingsSort.tsx
@@ -24,29 +24,31 @@ const BookingsSort = (props : BookingsSortProps) => {
     const options: JSX.Element[] = [];
     filters.forEach((val) => {
         options.push(
-            <button
+            <button className="text-white border-radius-30 w-180 h-50 s-14 m-10 bg-c2 as-center font-weight-700"
                 id={"1" + val.name}
                 onClick={() => {
                     props.close();
                     props.submit(val.name, "asc");
                 }}>
-                {val.visibleName}: {val.ascending}
+                {val.visibleName} {val.ascending}
             </button>)
         options.push(
-            <button
+            <button  className="text-white border-radius-30 w-180 h-50 s-14 m-10 bg-c2 as-center font-weight-700"
                 id={"2" + val.name}
                 onClick={() => {
                     props.close();
                     props.submit(val.name, "desc");
                 }}>
-                {val.visibleName}: {val.descending}
+                {val.visibleName} {val.descending}
             </button>)
     });
 
     return (
-        <div>
-            <button onClick={() => {props.close()}}>
-                <RiCloseLine className="icon" />
+        <div className="flex-col flex-ac-center flex-a-start p-20 w-200 pt-20">
+            <button className="bg-transparent btn-close" onClick={() => {
+                props.close()
+            }}>
+                <RiCloseLine className="icon"/>
             </button>
             {options}
         </div>
