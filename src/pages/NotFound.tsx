0e40e9338dca98edbf791b3c41405146f5350775
Magda <magdalenafalkowska00@wp.pl>
2022-01-31 12:43:01 +0100

    Not found styling

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <>
+        <div className="full-height flex-col flex-a-center flex-ac-center flex-j-center">
          Not found :(   
-        </>
+        </div>
     )
 }
 
